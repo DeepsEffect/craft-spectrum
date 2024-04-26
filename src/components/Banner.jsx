8beb8d5c1090ee95dfd0b4c0f8d876740bab6f10
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-04-26 20:10:23 +0600

    added autoplay to the caruosel

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,8 @@
 import { Carousel } from "@material-tailwind/react";
- 
- export default function Banner() {
+
+export default function Banner() {
   return (
-    <Carousel className="">
+    <Carousel className="" autoplay={true} autoplayDelay={4000} loop={true}>
       <img
         src="https://images.unsplash.com/photo-1607346705566-b2b5048ae44a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="image 1"
@@ -15,6 +15,11 @@ import { Carousel } from "@material-tailwind/react";
       />
       <img
         src="https://images.unsplash.com/photo-1614916872450-3a0130b8fd92?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="image 3"
+        className="h-[400px] lg:h-[800px] w-full object-cover"
+      />
+      <img
+        src="https://images.unsplash.com/photo-1581872553286-2746c6a8b295?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="image 3"
         className="h-[400px] lg:h-[800px] w-full object-cover"
       />
