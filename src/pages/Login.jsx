f0d20580f31b54d38238f3c67827a819a35ece53
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-04-26 08:23:37 +0600

    login and register toggle

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,51 @@
-const Login = () => {
-  return (
-    <div>
-      <h2>Login</h2>
-    </div>
-  );
-};
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Input,
+  Checkbox,
+  Button,
+} from "@material-tailwind/react";
+import { Link } from "react-router-dom";
 
+function Login() {
+  return (
+    <Card className=" w-full lg:w-96 border-2 mx-auto mt-12 lg:mt-20">
+      <CardHeader
+        variant="gradient"
+        color="gray"
+        className="mb-4 grid h-28 place-items-center"
+      >
+        <Typography variant="h3" color="white">
+          Sign In
+        </Typography>
+      </CardHeader>
+      <CardBody className="flex flex-col gap-4">
+        <Input label="Email" size="lg" />
+        <Input label="Password" size="lg" />
+        <div className="-ml-2.5">
+          <Checkbox label="Remember Me" />
+        </div>
+      </CardBody>
+      <CardFooter className="pt-0">
+        <Button variant="gradient" fullWidth>
+          Login
+        </Button>
+        <Typography variant="small" className="mt-6 flex justify-center">
+          Don&apos;t have an account?
+          <Link
+            to={'/register'}
+            variant="small"
+            color="blue-gray"
+            className="ml-1 font-bold"
+          >
+            Register
+          </Link>
+        </Typography>
+      </CardFooter>
+    </Card>
+  );
+}
 export default Login;
