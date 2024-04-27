d27104dd14fd37aa056c1a97cdcee3c6d85a7c59
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-04-27 13:10:25 +0600

    added homepage cards

--- src/components/CraftItems.jsx
+++ src/components/CraftItems.jsx
@@ -1,11 +1,15 @@
-import { CraftCards } from "./CraftCards";
+/* eslint-disable react/prop-types */
 
-const CraftItems = () => {
+import { Button } from "@material-tailwind/react";
+import { CraftCardHome } from "./CraftCardHome";
+import { Link } from "react-router-dom";
+
+const CraftItems = ({ crafts }) => {
   return (
     <div className="text-black">
-        {/* header section */}
+      {/* header section */}
       <section className="text-center max-w-md mx-auto mt-10">
-        <h2>Discover Our Handcrafted Treasures</h2>
+        <h2 className="text-2xl">Discover Our Handcrafted Treasures</h2>
         <p>
           Explore our curated collection of artisanal craft items, meticulously
           crafted to inspire and delight. From charming trinkets to stunning
@@ -15,13 +19,15 @@ const CraftItems = () => {
       </section>
       {/* cards section */}
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-8 gap-10 lg:px-40 ">
-        <CraftCards></CraftCards>
-        <CraftCards></CraftCards>
-        <CraftCards></CraftCards>
-        <CraftCards></CraftCards>
-        <CraftCards></CraftCards>
-        <CraftCards></CraftCards>
+        {crafts.map((craft) => (
+          <CraftCardHome craft={craft} key={craft._id}></CraftCardHome>
+        ))}
       </section>
+      <Link to={'/allArtAndCraft'}>
+        <Button className="mx-auto border flex justify-center mt-6">
+          View All Crafts
+        </Button>
+      </Link>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
+import { useLoaderData } from "react-router-dom";
 import Banner from "../components/Banner";
 import CraftItems from "../components/CraftItems";
 
 const Home = () => {
+  const crafts = useLoaderData()
+
   return (
     <div >
       <Banner></Banner>
-      <CraftItems></CraftItems>
+      <CraftItems crafts={crafts}></CraftItems>
     </div>
   );
 };
