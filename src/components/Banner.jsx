0aabcea53081bc7d593631b3332c99ffd0df097f
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-04-28 15:00:33 +0600

    banner update with text

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,28 +1,95 @@
-import { Carousel } from "@material-tailwind/react";
+import { Carousel, Typography } from "@material-tailwind/react";
 
 export default function Banner() {
   return (
-    <Carousel className="" autoplay={true} autoplayDelay={4000} loop={true}>
-      <img
-        src="https://images.unsplash.com/photo-1607346705566-b2b5048ae44a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="image 1"
-        className="h-[400px] lg:h-[800px] w-full object-cover"
-      />
-      <img
-        src="https://images.unsplash.com/photo-1525606846543-2b3c14f8e18f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="image 2"
-        className="h-[400px] lg:h-[800px] w-full object-cover"
-      />
-      <img
-        src="https://images.unsplash.com/photo-1614916872450-3a0130b8fd92?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="image 3"
-        className="h-[400px] lg:h-[800px] w-full object-cover"
-      />
-      <img
-        src="https://images.unsplash.com/photo-1581872553286-2746c6a8b295?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="image 3"
-        className="h-[400px] lg:h-[800px] w-full object-cover"
-      />
+    <Carousel className="" autoplay={false} autoplayDelay={4000} loop={true}>
+      {/* slider 1 */}
+      <div className="relative">
+        <img
+          src="https://images.unsplash.com/photo-1607346705566-b2b5048ae44a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="image 1"
+          className="h-[400px] lg:h-[800px] w-full object-cover"
+        />
+        <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/65">
+          <div className="w-3/4 text-center md:w-2/4">
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+            >
+              The Beauty of Nature
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="mb-12 opacity-80"
+            >
+              It is not so much for its beauty that the forest makes a claim
+              upon men&apos;s hearts, as for that subtle something, that quality
+              of air that emanation from old trees, that so wonderfully changes
+              and renews a weary spirit.
+            </Typography>
+          </div>
+        </div>
+      </div>
+      {/* slider 2 */}
+      <div className="relative">
+        <img
+          src="https://images.unsplash.com/photo-1525606846543-2b3c14f8e18f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="image 1"
+          className="h-[400px] lg:h-[800px] w-full object-cover"
+        />
+        <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/65">
+          <div className="w-3/4 text-center md:w-2/4">
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+            >
+              The Beauty of Nature
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="mb-12 opacity-80"
+            >
+              It is not so much for its beauty that the forest makes a claim
+              upon men&apos;s hearts, as for that subtle something, that quality
+              of air that emanation from old trees, that so wonderfully changes
+              and renews a weary spirit.
+            </Typography>
+          </div>
+        </div>
+      </div>
+      {/* slider 3 */}
+      <div className="relative">
+        <img
+          src="https://images.unsplash.com/photo-1581872553286-2746c6a8b295?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="image 1"
+          className="h-[400px] lg:h-[800px] w-full object-cover"
+        />
+        <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/65">
+          <div className="w-3/4 text-center md:w-2/4">
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+            >
+              The Beauty of Nature
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="mb-12 opacity-80"
+            >
+              It is not so much for its beauty that the forest makes a claim
+              upon men&apos;s hearts, as for that subtle something, that quality
+              of air that emanation from old trees, that so wonderfully changes
+              and renews a weary spirit.
+            </Typography>
+          </div>
+        </div>
+      </div>
     </Carousel>
   );
 }
