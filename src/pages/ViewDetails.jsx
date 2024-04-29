b73e5f36280d99f2cc3a66ab3720b12694989970
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-04-30 03:22:56 +0600

    view details made responsive

--- src/pages/ViewDetails.jsx
+++ src/pages/ViewDetails.jsx
@@ -30,16 +30,16 @@ const ViewDetails = () => {
       <h3 className="text-center text-4xl font-medium mt-20">
         Detailed View Of {item_name}
       </h3>
-      <Card className="w-full px-10 group max-w-5xl border border-primary bg-background flex-row items-center mx-auto mt-6">
+      <Card className="w-full lg:px-10 group max-w-5xl lg:border border-primary bg-background flex flex-col lg:flex-row items-center mx-auto mt-6">
         <CardHeader
           shadow={false}
           floated={false}
-          className="m-0 w-3/4 shrink-0 "
+          className="m-0 w-full lg:w-3/4 lg:shrink-0 "
         >
           <img
             src={image}
             alt="card-image"
-            className="w-full h-[500px] object-cover transition ease-in-out group-hover:scale-105"
+            className="w-full lg:h-[500px] object-cover transition ease-in-out group-hover:scale-105"
           />
         </CardHeader>
         <CardBody className="text-text dark:text-text">
